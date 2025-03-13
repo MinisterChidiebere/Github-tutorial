d79f1f7e0dfda35c2ea54f6542c12a1672aef598
ministerchidiebere <chidichidiebere77@gmail.com>
2025-03-13 12:06:50 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+class calculator:
+    def __init__(self):
+        self.operations = {
+            '+':self.add,
+            '-':self.subtract,
+            '*':self.multiply,
+            '/':self.divide,
+        }
+    
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y): 
+        return x - y
+    
+    def multiply(self, x, y): 
+        return x * y
+    
+    def divide(self, x, y): 
+        return x / y if y != 0 else 'Error: Division by zero'
+    
+    ## test case
+
+if __name__=='__main__':
+    calculator = calculator()
+    ## let add two numbers
+    x, y, = 10, 12
+    print(f"The addition of {x} + {y} = {calculator.add(x, y)}")
+    print(f"The subtraction of {x} - {y} = {calculator.subtract(x, y)}")
+    print(f"The multiplication of {x} * {y} = {calculator.multiply(x, y)}")
+    print(f"The division of {x} / {y} = {calculator.divide(x, y)}")
